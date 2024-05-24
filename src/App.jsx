e6b58697412419417afec09a630bd2516ca46914
Pradeep Kumar <pra45516@gmail.com>
2024-05-24 11:49:07 +0530

    Added homepage

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCard from "./components/HomeCard";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCard /> 
+      <JobListings/> 
+      <ViewAllJobs/>  
+    </>
+  );
+};
+
+export default App;
