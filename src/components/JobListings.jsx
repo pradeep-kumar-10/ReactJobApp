354c8479e0a727cf5fec247b28aec5afc94cd41c
Pradeep Kumar <pra45516@gmail.com>
2024-05-24 14:55:38 +0530

    Added json-server to fetch data

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,24 +1,49 @@
+import { useEffect, useState } from "react";
 import Job from "./Job";
-import jobs from "../jobs.json";
+import Spinner from "./Spinner";
 
 const JobListings = ({ isHome = false}) => {
-    const jobList = isHome? jobs.slice(0,3) : jobs;
+    const [jobs , setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        const apiURL = isHome ? '/api/jobs?_limit=3' : '/api/jobs'
+        const fetchJobs = async() => {
+            try{
+                const res = await fetch(apiURL);
+                const data = await res.json();
+                setJobs(data);
+            } catch(error){
+                console.log('Error in fetching data', error);
+            } finally{
+                setLoading(false);
+            }
+        }
+
+        fetchJobs();
+    },[])
+
     return (
         <section className ="bg-blue-50 px-4 py-10">
             <div className ="container-xl lg:container m-auto">
                 <h2 className ="text-3xl font-bold text-indigo-500 mb-6 text-center">
                     { isHome? "Recent Jobs" : "All Jobs"}
                 </h2>
-                <div className ="grid grid-cols-1 md:grid-cols-3 gap-6">
+                { loading ? (
+                    <Spinner loading = {loading} />
+                ) : (
+                    <div className ="grid grid-cols-1 md:grid-cols-3 gap-6">
                     {
-                        jobList.map((job) => (
+                        jobs.map((job) => (
                             <Job 
                                 key= {job.id}
                                 job = {job} 
                             />
                         ))
                     }
-                </div>
+                    </div>
+                )}
+ 
             </div>
         </section>
     )
