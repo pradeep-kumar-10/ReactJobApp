a0fdbd3e03550488999651b275346f380ccabb0d
Pradeep Kumar <pra45516@gmail.com>
2024-05-24 16:35:07 +0530

    Added job page with id search

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import JobsPages from "./pages/JobsPages";
 import NotFoundPage from "./pages/NotFoundPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={< MainLayout/>}>
       <Route index element={<HomePage />}/>
       <Route path="/jobs" element={<JobsPages />}/>
+      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader}/>
       <Route path="*" element={<NotFoundPage />}/>
     </Route>
   )
